ff6bde024871ed1e9d27d79ff4ee2a2aed0c6cb7
chalpin <chalpin@salesforce.com>
2015-07-31 10:12:21 -0400

    updates

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,7 +1,5 @@
-import React from 'react'
-import styles from './app.less'
+import React from 'react';
 
-<<<<<<< Updated upstream
 class Hello extends React.Component{
   render () {
     return <div className='container'>
@@ -9,13 +7,9 @@ class Hello extends React.Component{
           <h1 style={{fontWeight: '100', fontSize: '4rem', marginTop: '16rem'}}>boilerplate-hjs-webpack</h1>
         </header>
         <p>Check it out on <a href={'https://github.com/clintonhalpin/boilerplate-hjs-webpack'} style={{ color: 'white'}}>github</a> <br /><br/> built by <a href={'http://twitter.com/clintonhalpin'} style={{ color: 'white'}}>@clintonhalpin</a><br /><br />taken from <a href={'https://github.com/HenrikJoreteg/hjs-webpack/tree/master/examples/assets-and-index-html'} style={{ color: 'white'}}>hjs-webpack</a></p>
+        <p>See <a href={'/#/another'} style={{ color: 'white'}}>Another Route!</a></p>
       </div>
   }
 }
 
-React.render(<Hello/>, document.body)
-=======
-Router.run(routes, (Root, state) => {
-  React.render(<Root {...state} />, document.body);
-});
->>>>>>> Stashed changes
+export default Hello
