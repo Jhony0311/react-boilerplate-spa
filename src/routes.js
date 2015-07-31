0892e523e4f0a99e8f4a1f484b1cd938df4bd6dc
chalpin <chalpin@salesforce.com>
2015-07-31 07:51:47 -0400

    Add react router

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './components/Main';
+import Hello from './components/Hello';
+import Hello2 from './components/Hello2';
+import { Router, Route, DefaultRoute } from 'react-router';
+
+export default (
+  <Route name="app" path="/" handler={Main}>
+    <Route name="route2" path="another" handler={Hello2} />
+    <DefaultRoute handler={Hello} />
+  </Route>
+);
