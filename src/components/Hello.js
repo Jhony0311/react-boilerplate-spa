5acfce809aa06e968e1b711bc5837da2d620dbcc
chalpin <chalpin@salesforce.com>
2015-07-31 10:18:51 -0400

    Update react router link

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class Hello extends React.Component{
   render () {
@@ -7,7 +8,7 @@ class Hello extends React.Component{
           <h1 style={{fontWeight: '100', fontSize: '4rem', marginTop: '16rem'}}>boilerplate-hjs-webpack</h1>
         </header>
         <p>Check it out on <a href={'https://github.com/clintonhalpin/boilerplate-hjs-webpack'} style={{ color: 'white'}}>github</a> <br /><br/> built by <a href={'http://twitter.com/clintonhalpin'} style={{ color: 'white'}}>@clintonhalpin</a><br /><br />taken from <a href={'https://github.com/HenrikJoreteg/hjs-webpack/tree/master/examples/assets-and-index-html'} style={{ color: 'white'}}>hjs-webpack</a></p>
-        <p>See <a href={'/#/another'} style={{ color: 'white'}}>Another Route!</a></p>
+        <p>See <Link to='route2' style={{ color: 'white'}}>Another Route!</Link></p>
       </div>
   }
 }
