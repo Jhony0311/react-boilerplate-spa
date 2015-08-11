900ee837e1bfc3f651da4e6c33f3c1c807b1f77c
Clinton Halpin <clintonhalpin@gmail.com>
2015-08-10 21:12:17 -0400

    wut is app.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var getConfig = require('hjs-webpack')
 
 module.exports = getConfig({
-    in: 'src/app.js',
+    in: 'src/',
     out: 'dist',
     clearBeforeBuild: true,
     html: function (context) {
