b483f3964fa6a378c98640dce46b97755f5a7d99
Clinton Halpin <clintonhalpin@gmail.com>
2015-08-10 21:10:11 -0400

    Route's in its own folder

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Main from './containers/Main';
-import Hello from './components/Hello';
-import Hello2 from './components/Hello2';
+import Main from './../containers/Main';
+import Hello from './../components/Hello';
+import Hello2 from './../components/Hello2';
 import { Router, Route, DefaultRoute } from 'react-router';
 
 export default (
