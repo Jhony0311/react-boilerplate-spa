585c00e4aa39e232e873943ada847d682f4d7ed5
Clinton Halpin <clintonhalpin@gmail.com>
2015-12-30 21:56:01 -0500

    Partial Fixes

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -5,7 +5,7 @@ class Main extends React.Component{
   render(){
     return (
       <div>
-        <RouteHandler {...this.props}/>
+        {this.props.children}
       </div>
     )
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Main from './../containers/Main';
 import Hello from './../components/Hello';
 import Hello2 from './../components/Hello2';
-import { Router, Route, DefaultRoute } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 
 export default (
   <Router>
     <Route path="/" component={Main}>
-    	<Route name="route2" path="another" handler={Hello2} />
+    	<IndexRoute component={Hello}/>
+    	<Route path="another" handler={Hello2} />
     </Route>
   </Router>
 )
