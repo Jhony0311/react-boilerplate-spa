9d29a2cb9feec9cc4668ba86298ce91b4890dac9
Clinton Halpin <clintonhalpin@gmail.com>
2016-01-26 23:59:15 -0500

    Update

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -6,7 +6,7 @@ const Hello = () => (
         <header role='banner'>
           <h1 style={{fontWeight: '100', fontSize: '4rem', marginTop: '16rem'}}>boilerplate-hjs-webpack</h1>
         </header>
-        <p>Check it out on <a href={'https://github.com/clintonhalpin/boilerplate-hjs-webpack'} style={{ color: 'white'}}>github</a> <br /><br/> built by <a href={'http://twitter.com/clintonhalpin'} style={{ color: 'white'}}>@clintonhalpin</a><br /><br />taken from <a href={'https://github.com/HenrikJoreteg/hjs-webpack/tree/master/examples/assets-and-index-html'} style={{ color: 'white'}}>hjs-webpack</a></p>
+        <p>View on Github <a href={'https://github.com/clintonhalpin/boilerplate-hjs-webpack'} style={{ color: 'white'}}>github</a> <br /><br/> built by <a href={'http://twitter.com/clintonhalpin'} style={{ color: 'white'}}>@clintonhalpin</a><br /><br />taken from <a href={'https://github.com/HenrikJoreteg/hjs-webpack/tree/master/examples/assets-and-index-html'} style={{ color: 'white'}}>hjs-webpack</a></p>
         <p>See <Link to='/another' style={{ color: 'white'}}>Another Route!</Link></p>
     </div>
 );
